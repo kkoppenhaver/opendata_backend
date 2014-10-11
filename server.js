d6043e32032bdd8ad1da8429ef64fda897e5bdef
Keanan Koppenhaver <k.koppenhaver@gmail.com>
2014-10-11 15:37:58 -0500

    Very basic routing

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ app.get('/', function(req, res){
   res.send('Hello World!');
 });
 
+app.get('/yelp', function(req, res){
+  res.send('Yelp');
+});
+
+app.get('/opentable', function(req, res){
+  res.send('Opentable');
+});
+
+app.get('/grubhub', function(req, res){
+  res.send('Grubhub');
+});
+
+app.get('/groupon', function(req, res){
+  res.send('Groupon');
+});
+
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
 });
