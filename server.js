009229208d997e762273a58706f656752bcd5695
Keanan Koppenhaver <k.koppenhaver@gmail.com>
2014-10-11 16:21:35 -0500

    Adding getconfig and yelp config variables

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var config = require('getconfig');
+
+
+function parseYelp(restaurantName, zipcode) {
+	console.log(config.yelp.token);
+	return { "Yelp": true}
+}
 
 app.get('/get-restaurant', function(req, res){
-  data = { "name ": req.query.name, "price": req.query.zipcode };
+  yelpData = parseYelp(req.query.name, req.query.zipcode);
+  data = { "name ": req.query.name, "price": req.query.zipcode, "yelp": yelpData };
   res.json(data);
   res.end();
 });
