e9a7b183ba5b29f02a53e3f8349d0b79e0a5ea88
Keanan Koppenhaver <k.koppenhaver@gmail.com>
2014-10-11 17:25:30 -0500

    Adding port listener for heroku

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 var config = require('getconfig');
 
+app.listen(process.env.PORT || 3000);
+
 
 function parseYelp(restaurantName, zipcode) {
 	return { "Yelp": true}
 }
 
 app.get('/get-restaurant', function(req, res){
-  data = { "name ": req.query.name, "price": req.query.zipcode };
+  data = { "name ": req.query.name, "zipcode": req.query.zipcode };
   res.json(data);
   res.end();
 });
