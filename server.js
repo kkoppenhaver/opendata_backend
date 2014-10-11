0142019d87191b279b7b535cf606131234bbc68a
Keanan Koppenhaver <k.koppenhaver@gmail.com>
2014-10-11 17:20:09 -0500

    remove random git stuff

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var app = express();
-<<<<<<< HEAD
 var config = require('getconfig');
 
 
 function parseYelp(restaurantName, zipcode) {
 	return { "Yelp": true}
 }
-=======
->>>>>>> 665ab7f35e2bf0bee06216c6b32677a41bfd4c60
 
 app.get('/get-restaurant', function(req, res){
   data = { "name ": req.query.name, "price": req.query.zipcode };
