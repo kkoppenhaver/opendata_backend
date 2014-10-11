a63fcc9a82bd3ffe43b57c3c93095ae572f35c9a
Andy Kong <andy@andy-kong.com>
2014-10-11 18:23:03 -0500

    Implemented open data api and general structure

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
+var http = require("http");
 var express = require('express');
+var config = require('getConfig');
 var app = express();
+var request = require("request");
 
 app.get('/get-restaurant', function(req, res){
   data = { "name ": req.query.name, "price": req.query.zipcode };
@@ -8,33 +11,38 @@ app.get('/get-restaurant', function(req, res){
 });
 
 app.get('/get-opendata', function (req, res) {
-    data = { "name": req.query.name, "zip": req.query.zip };
-    res.json(data);
-    res.end();
+    dataIn = { "name": req.query.name, "zip": req.query.zip };
+    getOpenData(dataIn.name, dataIn.zip, res);
 });
 
-// get is a simple wrapper for request()
-// which sets the http method to GET
-var getOpenData = http.get(url, function (response) {
-    // data is streamed in chunks from the server
-    // so we have to handle the "data" event    
-    var buffer = "",
-        data,
-        route;
+var getOpenData = function (name, zip, response) {
+    var url = config.openData.url + "collections/restaurants?q={ \"DBA Name\": " + name + ", Zip: " + zip + "}&apiKey=" + config.openData.apiKey;
+    var openDataResults;
+    request.get(url, function (e, r, openDataResults) {
+        var data = JSON.parse(openDataResults);
+        var firstData = data[0];
 
-    response.on("data", function (chunk) {
-        buffer += chunk;
+        var output =
+        {
+            "Name": firstData["DBA Name"],
+            "Address1": firstData["Address"],
+            "City": firstData["City"],
+            "State": firstData["State"],
+            "Zip": firstData["Zip"],
+            "Risk": firstData["Risk"],
+            "Results": firstData["Results"],
+            "Latitude": firstData["Latitude"],
+            "Longitude": firstData["Longitude"],
+            "Location": firstData["Location"],
+            "InspectionViolations": firstData["Violations"],
+            "YelpRating": "",
+            "SearchCompleted": true,
+        };
+        console.log(output);
+        response.json(output);
+        response.end();
     });
-
-    response.on("end", function (err) {
-        // finished transferring data
-        // dump the raw data
-        console.log(buffer);
-        console.log("\n");
-        data = JSON.parse(buffer);
-        res.end(data);
-    });
-});
+};
 
 var server = app.listen(3000, function() {
     console.log('Listening on port %d', server.address().port);
